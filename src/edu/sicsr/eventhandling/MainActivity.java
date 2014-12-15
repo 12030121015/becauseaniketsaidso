6046ea62874146a90c68ce6af42b543f23162d0b
manshu9393 <12030121015@sicsr.ac.in>
2014-12-15 09:51:39 +0530

    create blank project

--- src/edu/sicsr/eventhandling/MainActivity.java
+++ src/edu/sicsr/eventhandling/MainActivity.java
@@ -1,15 +1,31 @@
 package edu.sicsr.eventhandling;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1=(Button) findViewById(R.id.button1);
+        OnClickListener listner =new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				button1.setText("Button was Click");
+				
+			}
+		};
+       button1.setOnClickListener(listner);
+       
     }
 
 
